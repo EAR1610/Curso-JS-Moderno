8af6645e95c40d642048e417b0af360dc4a99332
Alexander Reynoso Я <61547337+EAR1610@users.noreply.github.com>
2021-01-25 10:13:05 -0600

    Fechas
    Se realizó el capítulo de Fechas...

---
+++ 21-Fechas/js/01-app.js
@@ -0,0 +1,13 @@
+const diaHoy = new Date();
+
+let valor;
+
+valor = diaHoy;
+
+valor = diaHoy.getFullYear();
+valor = diaHoy.getMonth(); //Enero lo toma como 0, importante....
+valor = diaHoy.getMinutes();
+valor = diaHoy.getTime();
+
+
+console.log(valor);
