ce61b6e35c7ce2940b6a8b9919e75495996d61ab
Alexander Reynoso Я <61547337+EAR1610@users.noreply.github.com>
2021-03-24 09:30:35 -0600

    FetchAPI
    se realizó el capítulo de  FetchAPI

---
+++ 35-FetchAPI/js/02-app.js
@@ -0,0 +1,20 @@
+const cargarJsonBtn = document.querySelector('#cargarJSON');
+cargarJsonBtn.addEventListener('click', obtenerDatos);
+
+function obtenerDatos() {
+    const url = 'data/empleado.json' 
+    fetch(url)
+        .then( respuesta =>  respuesta.json() )
+        .then( resultado => mostrarHTML( resultado ) )
+}
+
+function mostrarHTML({ empresa, id, nombre, trabajo }) {
+    const contenido = document.querySelector('.contenido');
+    
+    contenido.innerHTML = `
+        <p>Empleado: ${nombre}</p>
+        <p>ID: ${id}</p>
+        <p>Empresa: ${empresa}</p>
+        <p>Trabajo: ${trabajo}</p>
+    `;
+}
---
+++ 35-FetchAPI/js/03-app.js
@@ -0,0 +1,29 @@
+const cargarJSONArray = document.querySelector('#cargarJSONArray');
+cargarJSONArray.addEventListener('click', obtenerDatos);
+
+function obtenerDatos() {
+    const url = 'data/empleados.json';
+
+    fetch(url)
+        .then( respuesta => respuesta.json() )
+        .then( resultado => mostrarHTML( resultado ) )
+}
+
+function mostrarHTML(empleados) {
+    const contenido = document.querySelector('.contenido');
+
+    let html = '';
+
+    empleados.forEach( empleado => {
+        const { id, nombre, empresa, trabajo } = empleado; 
+
+        html+= `
+            <p>Empleado: ${nombre}</p>
+            <p>ID: ${id}</p>
+            <p>Empresa: ${empresa}</p>
+            <p>Trabajo: ${trabajo}</p>
+        `;
+    });
+
+    contenido.innerHTML = html;
+}
