ddc03ae36fce8184f27f194462e74669ccc1167e
Alexander Reynoso Я <61547337+EAR1610@users.noreply.github.com>
2021-03-28 10:26:34 -0600

    Busca tu canción
    Se realizó el proyecto 'busca tu canción' con modulos!

---
+++ 37-PROYECTO-BuscarCanciones/js/api.js
@@ -0,0 +1,36 @@
+import * as UI from './interfaz.js';
+
+class API {
+    constructor(artista, cancion) {
+        this.artista = artista;
+        this.cancion = cancion;
+    } 
+
+    consultarAPI(){
+        const url = `https://api.lyrics.ovh/v1/${this.artista}/${this.cancion}`;
+
+        fetch(url)
+            .then( respuesta => respuesta.json() )
+            .then( resultado => {
+                if ( resultado.lyrics ) {
+                    const { lyrics } = resultado; //Destructuring a resultado
+                    UI.divResultado.textContent = lyrics;
+                    UI.headingResultado.textContent = `letra de la canción: ${this.cancion} de ${this.artista}`;
+                    UI.formularioBuscar.reset();
+                } 
+                
+            } )
+            .catch(error => {
+                UI.divMensajes.innerHTML = 'La canción no existe, prueba con otra busqueda';
+                UI.divMensajes.classList.add('error');
+                UI.formularioBuscar.reset();
+                setTimeout(() => {
+                    UI.divMensajes.innerHTML = '';
+                    UI.divMensajes.classList.remove('error');
+                    UI.formularioBuscar.reset();
+                }, 3000);
+            })            
+    }
+}
+
+export default API;
---
+++ 37-PROYECTO-BuscarCanciones/js/app.js
@@ -0,0 +1,31 @@
+import * as UI from './interfaz.js';
+import API from './api.js'
+
+UI.formularioBuscar.addEventListener('submit', buscarCancion);
+
+function buscarCancion(e) {
+    e.preventDefault();
+    UI.divResultado.textContent = '';
+    UI.headingResultado.textContent = '';
+
+    //Obtener datos del formulario
+    const artista = document.querySelector('#artista').value;
+    const cancion = document.querySelector('#cancion').value;
+
+    if (artista === '' || cancion === '') {
+        //El usuario dejo por lo menos un campo vacío, mostrar error
+        UI.divMensajes.textContent = 'Error... Todos los campos son obligatorios';
+        UI.divMensajes.classList.add('error');
+
+        setTimeout(() => {
+            UI.divMensajes.textContent = '';
+            UI.divMensajes.classList.remove('error');
+        }, 3000);
+
+        return;
+    }
+
+    //Consultar nuestra API
+    const busqueda = new API(artista, cancion);
+    busqueda.consultarAPI();
+}
