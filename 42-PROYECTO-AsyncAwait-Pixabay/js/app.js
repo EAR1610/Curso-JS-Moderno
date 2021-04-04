1ade6aff766e4628a26b75eaf3fc635410fc9536
Alexander Reynoso Я <61547337+EAR1610@users.noreply.github.com>
2021-04-04 15:02:11 -0600

    Async Await Projects
    Se movieron los proyectos de: Pixabay y criptomonedas a Async Await

--- 42-PROYECTO-AsyncAwait-Pixabay/js/app.js
+++ 42-PROYECTO-AsyncAwait-Pixabay/js/app.js
@@ -48,19 +48,28 @@ function mostrarAlerta(mensaje) {
 
 }
 
-function buscarImagenes() {
+async function buscarImagenes() {
 
     const termino = document.querySelector('#termino').value;
 
-    const key = '';
+    const key = '20900942-5790e4a057f5e75471ea56c27';
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
 
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(resultado => {
-            totalPaginas = calcularPaginas(resultado.totalHits);
-            mostrarImagenes(resultado.hits);
-        })
+    // fetch(url)
+    //     .then(respuesta => respuesta.json())
+    //     .then(resultado => {
+    //         totalPaginas = calcularPaginas(resultado.totalHits);
+    //         mostrarImagenes(resultado.hits);
+    //     })
+
+    try {
+        const respuesta = await fetch( url );
+        const resultado = await respuesta.json();
+        totalPaginas = calcularPaginas(resultado.totalHits);
+        mostrarImagenes(resultado.hits);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 // Generador que va a registrar la cantidad de elementos de acuerdo a las paginas
